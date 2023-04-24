bc6c35ff7550b75c3f328cd1d752ea19efd6f040
Vitorakavitor <49196415+Vitorakavitor@users.noreply.github.com>
2023-04-24 20:33:14 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
+function AboutPage() {
+  const [cep, setCep] = useState('')
+  const [endereco, setEndereco] = useState({})
+
+  function handleSearch() {
+    fetch(`https://viacep.com.br/ws/${cep}/json/`)
+      .then(response => response.json())
+      .then(data => setEndereco(data))
+      .catch(error => console.log(error))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="iphone-xs">
+      <div className="header">
+        <h1 className="title">Sobre mim</h1>
+      </div>
+      <div className="content">
+        <div className="form-group">
+          <label htmlFor="cep">Meu CEP:</label>
+          <input
+            type="text"
+            id="cep"
+            placeholder="Digite o CEP aqui"
+            value={cep}
+            onChange={event => setCep(event.target.value)}
+          />
+          <button onClick={handleSearch}>Buscar</button>
+        </div>
+        {endereco.logradouro && (
+          <div className="endereco">
+            <p>
+              <strong>Endereço:</strong> {endereco.logradouro}
+            </p>
+            <p>
+              <strong>Bairro:</strong> {endereco.bairro}
+            </p>
+            <p>
+              <strong>Cidade:</strong> {endereco.localidade} - {endereco.uf}
+            </p>
+          </div>
+        )}
+        {endereco.erro && (
+          <div className="erro">
+            <p>
+              O CEP informado não foi encontrado. Por favor, verifique se o CEP
+              está correto e tente novamente.
+            </p>
+          </div>
+        )}
+        {!endereco.logradouro && !endereco.erro && (
+          <div className="instrucoes">
+            <p>
+              Digite o seu CEP acima para descobrir o seu endereço completo.
+            </p>
+          </div>
+        )}
+      </div>
+      <div className="footer">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Desenvolvido por <a href="https://example.com">Seu nome aqui</a> -
+          Todos os direitos reservados © 2023
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default AboutPage
